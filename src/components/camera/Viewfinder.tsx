6760510f8de3a2f0e69e5e63a257325bb41c2ea4
ecoactivators <danielle@ecoactivators.com>
2025-08-27 21:51:20 -0400

    Updated Viewfinder.tsx

--- src/components/camera/Viewfinder.tsx
+++ src/components/camera/Viewfinder.tsx
@@ -62,7 +62,7 @@ export const Viewfinder: React.FC = () => {
   }, [permissionState.granted]);
 
   const handleSnap = async () => {
-    console.log('📸 [Snap] Snap triggered!', { 
+    console.log('📸 [Snap] Snap triggered! 1', { 
       location,
       cameraGranted: permissionState.granted,
       videoElement: !!videoRef.current,
@@ -70,6 +70,13 @@ export const Viewfinder: React.FC = () => {
     });
     
     const snapResult = await triggerSnap();
+
+    console.log('📸 [Snap] Snap triggered! 2', { 
+      location,
+      cameraGranted: permissionState.granted,
+      videoElement: !!videoRef.current,
+      videoSrcObject: !!videoRef.current?.srcObject
+    });
     
     if (snapResult) {
       console.log('📸 [Snap] Snap successful, transitioning to analyze...', {
@@ -79,9 +86,23 @@ export const Viewfinder: React.FC = () => {
       });
       setCapturedImageData(snapResult.imageData || null);
       recordSnapSuccess();
+
+      console.log('📸 [Snap] Snap triggered! 3', { 
+      location,
+      cameraGranted: permissionState.granted,
+      videoElement: !!videoRef.current,
+      videoSrcObject: !!videoRef.current?.srcObject
+    });
       
       // Transition to analyze view
       setShowAnalysisView(true);
+
+      console.log('📸 [Snap] Snap triggered! 4', { 
+      location,
+      cameraGranted: permissionState.granted,
+      videoElement: !!videoRef.current,
+      videoSrcObject: !!videoRef.current?.srcObject
+    });
       
       // Start analysis with image and location
       if (snapResult.imageData) {
