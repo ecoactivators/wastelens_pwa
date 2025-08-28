1a9f07d31a027c207d908514b4259c3944555260
ecoactivators <danielle@ecoactivators.com>
2025-08-27 22:02:43 -0400

    Updated useWasteAnalysis.ts

--- src/hooks/useWasteAnalysis.ts
+++ src/hooks/useWasteAnalysis.ts
@@ -12,6 +12,12 @@ export const useWasteAnalysis = () => {
     setError(null);
     setAnalysisResult(null);
 
+    console.log('📸 [useWasteAnalysis] analyzeWaste()', { 
+      videoElement: !!videoRef.current,
+      videoSrcObject: !!videoRef.current?.srcObject
+    });
+
+    // %%%
     try {
       const result = await openAIService.analyzeWasteImage(imageData, location);
       setAnalysisResult(result);
