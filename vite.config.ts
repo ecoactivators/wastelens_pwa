60ce59b3f05e003ae4e62cf0711e109241441471
ecoactivators <dan@ecoactivators.com>
2025-10-07 23:09:00 -0400

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
-  plugins: [react(), mkcert()],
+  plugins: [react()],
   build: {
     outDir: 'dist',
     sourcemap: false,
@@ -14,7 +13,6 @@ export default defineConfig({
     },
   },
   server: {
-    https: true,
     host: '0.0.0.0',
   },
 });
