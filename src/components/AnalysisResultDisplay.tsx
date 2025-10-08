8a0f7582d36cf294645e599f63742bcad813f9d2
Dan McCoy <spotterup@protonmail.com>
2025-10-08 09:53:07 -0400

    Attempting to handle unwanted dragging

--- src/components/AnalysisResultDisplay.tsx
+++ src/components/AnalysisResultDisplay.tsx
@@ -138,7 +138,13 @@ export const AnalysisResultDisplay: React.FC<AnalysisResultDisplayProps> = ({
     <div className="relative w-screen h-screen overflow-hidden">
       {/* Background Image - Fixed Position */}
       {capturedImage && (
-        <div className="fixed inset-0 w-full h-full z-0">
+        <div
+            className="fixed inset-0 w-full h-full z-0 select-none"
+            style={{ WebkitUserDrag: 'none', userSelect: 'none', touchAction: 'none' } as React.CSSProperties}
+            onDragStart={(e) => e.preventDefault()}
+            onTouchStart={(e) => e.preventDefault()}
+            onMouseDown={(e) => e.preventDefault()}
+        >
           <img
             src={capturedImage}
             alt="Captured waste"
@@ -199,16 +205,14 @@ export const AnalysisResultDisplay: React.FC<AnalysisResultDisplayProps> = ({
             isDragging ? '' : 'transition-all duration-300 ease-out'
           }`}
           style={{ height: `${cardHeight}vh` }}
+          onTouchStart={handleTouchStart}
+          onTouchMove={handleTouchMove}
+          onTouchEnd={handleTouchEnd}
+          onMouseDown={handleMouseDown}
         >
           {/* Drag Handle */}
-          <div
-            className="flex justify-center pt-3 pb-2 cursor-grab active:cursor-grabbing"
-            onTouchStart={handleTouchStart}
-            onTouchMove={handleTouchMove}
-            onTouchEnd={handleTouchEnd}
-            onMouseDown={handleMouseDown}
-          >
-            <div className="w-12 h-1 bg-gray-400 rounded-full"></div>
+          <div className="flex justify-center pt-3 pb-2">
+            <div className="w-12 h-1 bg-gray-400 rounded-full cursor-grab active:cursor-grabbing"></div>
           </div>
 
           {/* Scrollable Content */}
